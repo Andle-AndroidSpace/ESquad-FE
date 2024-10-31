63dae472b29fb9a06f690ed150abe28c3b08be6d
yejeii <yesie0108@gmail.com>
2024-10-31 19:06:53 +0900

    add: 팀 관련 파일 생성

---
+++ src/pages/team/StudyDetailPage.jsx
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import {
+    Box,
+    Typography,
+    Dialog,
+    DialogContent,
+    Card,
+    CardContent,
+    CardActions,
+    Grid,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    IconButton,
+    Avatar,
+    Divider,
+    Fab, TextField, Button,
+} from '@mui/material';
+import { alpha, useTheme } from '@mui/material';
+import {useLocation, useParams} from "react-router-dom";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import AddIcon from '@mui/icons-material/Add';
+import bookProfileManAndSea from '../../assets/book-profile-man-and-sea.jpg';
+
+const StudyDetailPage = ({ isSmallScreen, isMediumScreen }) => {
+    const theme = useTheme();
+    const location = useLocation();
+    const study = location.state.study;
+    const params = useParams();
+    const [isStudyDeleteModalOpen, setIsStudyDeleteModalOpen] = useState(false);
+    const [attachments, setAttachments] = useState(study.attachments || []);
+
+    const handleDeleteButtonClick = () => {
+        setIsStudyDeleteModalOpen(true);
+    };
+
+    const handleCloseStudyDeleteModal = () => {
+        setIsStudyDeleteModalOpen(false);
+    };
+
+    const handleAddAttachment = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            setAttachments([...attachments, { name: file.name }]);
+        }
+    }
+
+    const handleDeleteAttachment = (index) => {
+        const updatedAttachments = attachments.filter((_, i) => i !== index);
+        setAttachments(updatedAttachments);
+    };
+
+    return (
+        <Box
+            sx={{
+                border: '1px solid',
+                display: 'flex',
+                flexDirection: 'column',
+                // justifyContent: 'center',
+                // alignItems: 'center',
+                mb: 3,
+                p: 2,
+                gap: 2,
+            }}
+        >
+            <Box
+                sx={{
+                    width: '100%',
+                    height: '20vh',
+                    overflowY: 'auto',
+                }}
+            >
+                <Box
+                    component="img"
+                    src={study.bookImage || bookProfileManAndSea}
+                    alt={study.title}
+                    sx={{
+                        width: '100%',
+                        objectFit: 'contain',
+                        mb: 2,
+                    }}
+                />
+            </Box>
+            <Divider sx={{borderBottom: '1px solid #ddd'}} />
+
+            {/* Study Data Section */}
+            <Box sx={{ my: 4 }}>
+                <Typography variant="h5" fontWeight="bold" gutterBottom>
+                    {study.title}
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary" gutterBottom>
+                    {study.members}
+                </Typography>
+            </Box>
+
+            {/* Attachments Section */}
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>공유파일 리스트</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+
+                    <Box sx={{ mb: 2, display: 'flex', gap: 2 }}>
+                        <Button
+                            variant="contained"
+                            component="label"
+                            color="primary"
+                            onClick={handleAddAttachment}
+                        >
+                            파일 추가
+                            <input
+                                type="file"
+                                hidden
+                                onChange={handleAddAttachment}
+                            />
+                        </Button>
+                    </Box>
+                    <List>
+                        {attachments && attachments.length > 0 ? (
+                            attachments.map((attachment, index) => (
+                                <ListItem key={index}>
+                                    <ListItemIcon>
+                                        <AttachFileIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary={attachment.name} />
+                                    <IconButton edge="end" aria-label="delete" onClick={() => handleDeleteAttachment(index)}>
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </ListItem>
+                            ))
+                        ) : (
+                            <Typography variant="body2" color="textSecondary">
+                                첨부파일이 없습니다.
+                            </Typography>
+                        )}
+                    </List>
+                </AccordionDetails>
+            </Accordion>
+
+            {/* Post Creation Modal */}
+            {/*<StudyCreatgionDialog*/}
+            {/*    open={isStudyModalOpen}*/}
+            {/*    onClose={handleCloseStudyModal}*/}
+            {/*    selectedTeamId={params.teamId}*/}
+            {/*    selectedBook={book}*/}
+            {/*/>*/}
+        </Box>
+    );
+};
+
+export default StudyDetailPage;
