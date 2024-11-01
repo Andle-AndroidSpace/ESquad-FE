f0c703b003a4b9f5f120ddb1747b002edf1aaa60
wive10 <70935978+wive10@users.noreply.github.com>
2024-11-01 17:21:36 +0900

    refactor: design

--- src/pages/team/StudyCreationPage.jsx
+++ src/pages/team/StudyCreationPage.jsx
@@ -1,14 +1,103 @@
 import React, { useState } from 'react';
-import { Box, Button, Typography, InputBase, Divider, Chip, TextField, Select, MenuItem, Grid } from '@mui/material';
+import {
+    Box,
+    Button,
+    Typography,
+    TextField,
+    Grid,
+    FormControl,
+    Select,
+    MenuItem,
+    Divider
+} from '@mui/material';
 import { useTheme } from '@mui/material';
 import dayjs from 'dayjs';
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
-const StudyCreationPage = ({ onCancel, selectedTeamId, selectedBook }) => {
+const StudyCreationPage = ({ onCancel, selectedBook }) => {
+    const param = useParams();
     const theme = useTheme();
-    const formatDate = (date) => dayjs(date).format('YYYY/MM/DD');
+    const [formData, setFormData] = useState({
+        studyPageName: "",
+        startDate: "",
+        endDate: "",
+        description: "",
+    });
+
+    const [errorMessage, setErrorMessage] = useState("");
+    const [reminds, setReminds] = useState([]);
+    const userIds = ["", ""]; // 사용자 ID 배열
+    const navigate = useNavigate(); // useNavigate 훅 사용
+
+    const handleChange = (field, value) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [field]: value,
+        }));
+    };
+
+    const handleRemindChange = (index, field, value) => {
+        const updatedReminds = [...reminds];
+        updatedReminds[index] = {
+            ...updatedReminds[index],
+            [field]: value,
+        };
+        setReminds(updatedReminds);
+    };
+
+    const handleAddRemind = () => {
+        setReminds([...reminds, { dayType: "", timeAt: "", description: "" }]);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // 폼 제출 이벤트 방지
+        console.log("생성 버튼을 눌렀습니다.");
+        console.log(`책정보: ${JSON.stringify(selectedBook, null, 2)}`);
+        console.log(`폼 데이터: ${JSON.stringify(formData, null, 2)}`);
+        console.log(`리마인드: ${JSON.stringify(reminds, null, 2)}`);
+        console.log(`사용자 ID: ${JSON.stringify(userIds, null, 2)}`);
+
+        localStorage.removeItem("book");
+        if (!selectedBook) {
+            console.error("selectedBook 값이 설정되지 않았습니다.");
+            return;
+        }
+
+        const studyPageDto = {
+            bookDto: selectedBook,
+            studyInfoDto: formData,
+            reminds,
+            userIds,
+        };
+
+        try {
+            const response = await axios.post(`/api/${param.teamId}/study-pages`, studyPageDto);
+            const study = response.data; // API 응답에서 study 데이터 추출
+
+            // 리다이렉트하면서 study 상태 전달
+            navigate(`/teams/${param.teamId}/study/${study.id}`, { state: { study } });
+        } catch (error) {
+            console.error("Error creating study page:", error);
+            setErrorMessage("An error occurred while creating the study page. Please try again.");
+        }
+    };
+
+    const AlertDayOptions = [
+        { label: "Monday", value: 0 },
+        { label: "Tuesday", value: 1 },
+        { label: "Wednesday", value: 2 },
+        { label: "Thursday", value: 3 },
+        { label: "Friday", value: 4 },
+        { label: "Saturday", value: 5 },
+        { label: "Sunday", value: 6 },
+    ];
 
     return (
         <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
             sx={{
                 display: 'flex',
                 flexDirection: 'column',
@@ -38,7 +127,7 @@ const StudyCreationPage = ({ onCancel, selectedTeamId, selectedBook }) => {
                 </Grid>
 
                 {/* Book Info Column */}
-                <Grid item xs={12} md={8} sx={{ display: 'flex', flexDirection: 'column', gap: 1}}>
+                <Grid item xs={12} md={8} sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
                     <Typography variant="h5" fontWeight="bold" gutterBottom>
                         {selectedBook.title}
                     </Typography>
@@ -57,11 +146,12 @@ const StudyCreationPage = ({ onCancel, selectedTeamId, selectedBook }) => {
                         스터디 설정
                     </Typography>
                     <TextField
+                        value={formData.studyPageName}
+                        onChange={(e) => handleChange("studyPageName", e.target.value)}
                         fullWidth
                         size='small'
                         label="스터디 페이지 이름을 작성해주세요"
                         variant="outlined"
-                        // sx={{ mb: 2 }}
                     />
                 </Grid>
             </Grid>
@@ -69,46 +159,63 @@ const StudyCreationPage = ({ onCancel, selectedTeamId, selectedBook }) => {
             {/* Study Creation Form */}
             <Grid container spacing={4}>
                 {/* Study Duration Column */}
-                <Grid item xs={12} md={6} >
+                <Grid item xs={12} md={6}>
                     <Typography variant="subtitle1" fontWeight="bold" gutterBottom>
                         스터디 기간
                     </Typography>
                     <Box sx={{ display: 'flex', gap: 2, }}>
                         <TextField
+                            value={formData.startDate}
+                            onChange={(e) => handleChange("startDate", e.target.value)}
                             size="small"
                             type="date"
                             variant="outlined"
-                            defaultValue={formatDate('2024-08-19')}
-                            sx={{ width: '100%'}}
+                            sx={{ width: '100%' }}
                         />
                         <Typography variant="h6">~</Typography>
                         <TextField
+                            value={formData.endDate}
+                            onChange={(e) => handleChange("endDate", e.target.value)}
                             size="small"
                             type="date"
                             variant="outlined"
-                            defaultValue={formatDate('2024-08-19')}
                             sx={{ width: '100%' }}
                         />
                     </Box>
                 </Grid>
 
-                {/* Study Schedule Column */}
+                {/* Study Schedule Section */}
                 <Grid item xs={12} md={6}>
                     <Typography variant="subtitle1" fontWeight="bold" gutterBottom>
                         스터디 요일 및 시간
                     </Typography>
-                    <Box sx={{ display: 'flex', gap: 2 }} >
-                        <Select fullWidth defaultValue="월요일" size="small">
-                            <MenuItem value="월요일">월요일</MenuItem>
-                            <MenuItem value="화요일">화요일</MenuItem>
-                            <MenuItem value="수요일">수요일</MenuItem>
-                            <MenuItem value="목요일">목요일</MenuItem>
-                            <MenuItem value="금요일">금요일</MenuItem>
-                            <MenuItem value="토요일">토요일</MenuItem>
-                            <MenuItem value="일요일">일요일</MenuItem>
-                        </Select>
-                        <TextField type="time" defaultValue="14:00" fullWidth size="small" />
-                    </Box>
+                    {reminds.map((remind, index) => (
+                        <Box key={index} sx={{ display: 'flex', gap: 2 }}>
+                            <FormControl>
+                                <Select
+                                    size="small"
+                                    value={remind.dayType}
+                                    onChange={(e) => handleRemindChange(index, "dayType", e.target.value)}
+                                    label="Date Type"
+                                >
+                                    {AlertDayOptions.map((option) => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            <TextField
+                                type="time"
+                                defaultValue="14:00"
+                                fullWidth
+                                size="small"
+                            />
+                        </Box>
+                    ))}
+                    <Button onClick={handleAddRemind} variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
+                        Add Reminder
+                    </Button>
                 </Grid>
 
                 {/* Study Introduction Column */}
@@ -117,6 +224,8 @@ const StudyCreationPage = ({ onCancel, selectedTeamId, selectedBook }) => {
                         스터디 페이지 개요
                     </Typography>
                     <TextField
+                        value={formData.description}
+                        onChange={(e) => handleChange("description", e.target.value)}
                         fullWidth
                         multiline
                         rows={11}
@@ -135,10 +244,10 @@ const StudyCreationPage = ({ onCancel, selectedTeamId, selectedBook }) => {
                     px: 1,
                 }}
             >
-                <Button variant="contained" onClick={onCancel} sx={{ color: '#fff', backgroundColor: theme.palette.warning.main, px: 4}}>
+                <Button variant="contained" onClick={onCancel} sx={{ color: '#fff', backgroundColor: theme.palette.warning.main, px: 4 }}>
                     취소
                 </Button>
-                <Button variant="contained" sx={{ backgroundColor: theme.palette.primary.main, color: '#fff', px: 4 }}>
+                <Button type="submit" variant="contained" sx={{ backgroundColor: theme.palette.primary.main, color: '#fff', px: 4 }}>
                     생성
                 </Button>
             </Box>
